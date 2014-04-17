a40fef979734541c6991e0033c6e69f1ebbff996
akim330 <akim330@gmail.com>
2014-04-17 14:58:05 -0400

    blah

---
+++ interface.py
@@ -0,0 +1,22 @@
+class Layer(object):
+	def 
+
+class Network(object):
+	def __init_ _(self, """parameters"""):
+		pass
+
+	def getstrength(self,):
+		pass
+
+	def setstrength(self,):
+
+	def makehiddennode(self,):
+
+	def feedforward(self,):
+
+	def backPropagate
+
+class Train(object):
+
+
+
